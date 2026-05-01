17747de3fa46f05946457ceab0dd8c95544d2f7f
diya <diyashakya01@gmail.com>
2026-05-01 18:31:54 +0545

    feat: Phase 1 completed :Environment and Django setup.

---
+++ evoting_ids/accounts/models.py
@@ -0,0 +1,15 @@
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser 
+from django.db import models
+                                                                                                            
+class CustomUser(AbstractUser):
+      ROLE_CHOICES = [                                                                                      
+          ('voter',       'Voter'),                                                                       
+          ('admin',       'Admin'),
+          ('soc_analyst', 'SOC Analyst'),
+      ]
+      role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='voter')
+
+      def __str__(self):
+          return f"{self.username} ({self.role})"
