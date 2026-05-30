43417c065a76ffddb1f49f40e8b5351468ba17aa
diya <diyashakya01@gmail.com>
2026-05-30 15:30:19 +0545

    Fixed the redirection of url

--- evoting_ids/evoting_ids/urls.py
+++ evoting_ids/evoting_ids/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('vote/', include('voting.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('api/', include('dashboard.urls')), 
+    path('api/', include('dashboard.urls')),
+    path('', RedirectView.as_view(url='/accounts/register/', permanent=False)),
 ]
