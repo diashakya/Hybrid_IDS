88959ded6d8f048b820607eac4e59adb21ce1655
diya <diyashakya01@gmail.com>
2026-06-13 16:08:52 +0545

    Modified ids_engine/admin.py

--- evoting_ids/ids_engine/admin.py
+++ evoting_ids/ids_engine/admin.py
@@ -1,6 +1,42 @@
+"""
 from django.contrib import admin
 
 # Register your models here.
 from .models import SecurityEvent, BlockedIP   
 admin.site.register(SecurityEvent)
 admin.site.register(BlockedIP) 
+"""
+from django.contrib import admin
+from ids_engine.models import SecurityEvent, BlockedIP
+
+
+@admin.register(SecurityEvent)
+class SecurityEventAdmin(admin.ModelAdmin):
+    list_display = [
+        'timestamp', 'event_type', 'ip_address',
+        'rule_score', 'ml_anomaly_score', 'risk_score',
+        'risk_level', 'action_taken', 'iso_control'
+    ]
+    list_filter  = ['risk_level', 'action_taken', 'event_type']
+    search_fields = ['ip_address', 'rule_name', 'iso_control']
+    ordering     = ['-timestamp']
+
+    readonly_fields = [
+        'timestamp', 'event_type', 'user', 'ip_address',
+        'user_agent', 'session_id', 'endpoint', 'method',
+        'status_code', 'request_payload', 'rule_triggered',
+        'rule_name', 'rule_score', 'ml_anomaly_score',
+        'risk_score', 'risk_level', 'action_taken',
+        'iso_control', 'nist_category'
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(BlockedIP)
+class BlockedIPAdmin(admin.ModelAdmin):
+    list_display = ['ip_address', 'reason']
